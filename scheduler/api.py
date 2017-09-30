34c9755eebb5a0e66b06f6c4f4909943df1e36d0
Douglas Orr <douglas.orr99@gmail.com>
2017-09-30 17:51:59 +0100

    Auth & Google Sheets endpoints

--- scheduler/api.py
+++ scheduler/api.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, Response
 from redis import Redis
+from oauth2client import client
+from apiclient import discovery
+import functools
+import flask
+import json
+import uuid
+import httplib2
 
 app = Flask(__name__)
+app.secret_key = str(uuid.uuid4())
 redis = Redis(host='redis', port=6379)
-
 root = "/api/1.0"
 import json
 
@@ -99,3 +106,99 @@ def person(id):
         return Response("OK", status=200)
     else:
         return Response("Invalid Method", status=400)
+
+
+### Adapter: google_sheet
+
+google_root = '/google/1.0'
+
+
+def with_google_auth(handler):
+    @functools.wraps(handler)
+    def decorated_function(*args, **kwargs):
+        if 'google_credentials' not in flask.session:
+            flask.session['google_oauth2_redirect'] = request.full_path
+            return flask.redirect(flask.url_for('google_oauth2'))
+
+        credentials = client.OAuth2Credentials.from_json(
+            flask.session['google_credentials'])
+        if credentials.access_token_expired:
+            flask.session['google_oauth2_redirect'] = request.full_path
+            return flask.redirect(flask.url_for('google_oauth2'))
+
+        http_auth = credentials.authorize(httplib2.Http())
+        return handler(*args, http_auth=http_auth, **kwargs)
+    return decorated_function
+
+
+@app.route(google_root + '/oauth2')
+def google_oauth2():
+    flow = client.flow_from_clientsecrets(
+        'client_secrets.json',
+        scope=('https://www.googleapis.com/auth/spreadsheets.readonly'
+               ' https://www.googleapis.com/auth/drive.metadata.readonly'),
+        redirect_uri=flask.url_for('google_oauth2', _external=True))
+
+    if 'code' not in request.args:
+        # Step 1.
+        return flask.redirect(flow.step1_get_authorize_url())
+
+    else:
+        # Step 2.
+        auth_code = request.args.get('code')
+        credentials = flow.step2_exchange(auth_code)
+        flask.session['google_credentials'] = credentials.to_json()
+
+        # Finished - go to the target URL
+        return flask.redirect(flask.session['google_oauth2_redirect'])
+
+
+@app.route(google_root + '/sheets')
+@with_google_auth
+def google_sheets(http_auth):
+    service = discovery.build('drive', 'v3', http=http_auth)
+
+    response = service.files().list(
+        q='mimeType="application/vnd.google-apps.spreadsheet"',
+        orderBy='modifiedTime desc',
+    ).execute()
+
+    return json.dumps([
+        dict(name=x['name'], id=x['id'])
+        for x in response['files']
+    ])
+
+
+@app.route(google_root + '/sheets/<id>/subsheets')
+@with_google_auth
+def google_sheets_subsheets(id, http_auth):
+    service = discovery.build('sheets', 'v4', http=http_auth)
+
+    response = service.spreadsheets().get(spreadsheetId=id).execute()
+
+    return json.dumps([
+        dict(name=p['title'], id=p['sheetId'])
+        for p in (x['properties'] for x in response['sheets'])
+        if p['sheetType'] == 'GRID'
+    ])
+
+
+@app.route(google_root + '/sheets/<id>/subsheets/<int:subsheet_id>/import',
+           methods=['GET'])
+@with_google_auth
+def google_sheets_subsheets_import(id, subsheet_id, http_auth):
+    service = discovery.build('sheets', 'v4', http=http_auth)
+
+    response = service.spreadsheets().get(
+        spreadsheetId=id,
+        includeGridData=True
+    ).execute()
+
+    row_data = response['sheets'][subsheet_id]['data'][0]['rowData']
+
+    values = [[cell.get('formattedValue') for cell in row['values']]
+              for row in row_data]
+
+    # Import code here!
+
+    return json.dumps(values)
