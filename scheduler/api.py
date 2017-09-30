b14c92de64f300b2e3804ad403ac4bfb827ed2d9
codehackdays <codehackdays@users.noreply.github.com>
2017-09-30 14:37:39 +0100

    Added basic GET endpoints

--- scheduler/api.py
+++ scheduler/api.py
@@ -6,22 +6,44 @@ redis = Redis(host='redis', port=6379)
 
 root = "/api/1.0"
 
+
 @app.route('/')
 def hello():
     count = redis.incr('hits')
     return 'Hello World! I have been seen {} times.\n'.format(count)
 
 
+#GET ALL
 @app.route(root + "/events")
 def events():
-    return "Event List"
+    return "Event: List"
 
 
+#GET ONE
+@app.route(root + "/events/<id>")
+def event(id):
+    return "Event: " + id
+
+
+#GET ALL
 @app.route(root + "/rotas")
 def rotas():
-    return "Rota List"
+    return "Rota: List"
 
 
+#GET ONE
+@app.route(root + "/rotas/<id>")
+def rota(id):
+    return "Rota: " + id
+
+
+#GET ALL
 @app.route(root + "/people")
 def people():
-    return "Person List"
+    return "Person: List"
+
+
+#GET ONE
+@app.route(root + "/people/<id>")
+def person(id):
+    return "Person: " + id
