c85ce0e8cffa45fad8622b3262bb89d9b654cd66
codehackdays <codehackdays@users.noreply.github.com>
2017-09-30 14:25:49 +0100

    Added basic resource endpoints

--- scheduler/api.py
+++ scheduler/api.py
@@ -4,7 +4,24 @@ from redis import Redis
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
+root = "/api/1.0"
+
 @app.route('/')
 def hello():
     count = redis.incr('hits')
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
+
+@app.route(root + "/events")
+def events():
+    return "Event List"
+
+
+@app.route(root + "/rotas")
+def rotas():
+    return "Rota List"
+
+
+@app.route(root + "/people")
+def people():
+    return "Person List"
